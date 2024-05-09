6b76bb8ded7e702080118dccf60b4c51f1c611a6
shoegaev <kshoegaev@gmail.com>
2024-05-09 18:37:25 +0300

    feat: implement drag drop

--- src/view/game-page/game-field/game-field-view.ts
+++ src/view/game-page/game-field/game-field-view.ts
@@ -3,7 +3,7 @@ import { ElementParametrs } from "../../../types/view-types";
 import { Loader } from "../../../loader/loader";
 import { ResultPanelView } from "./result-panel/result-panel-view";
 import { WordsPanelView } from "./words-panel/words-panel";
-import { AnimationMaker, InsertMethods } from "../../../util/animation-maker";
+import { AnimationMaker, InsertOptions } from "../../../util/animation-maker";
 import "./game-field-style.scss";
 
 export class GameFieldView extends ViewLoadable {
@@ -11,17 +11,28 @@ export class GameFieldView extends ViewLoadable {
 
   wordsPanelView: WordsPanelView;
 
-  panelViews: [ResultPanelView | null, WordsPanelView | null];
+  wordsLines: HTMLElement[];
+
+  resultLines: HTMLElement[];
+
+  wordsActiveLine: HTMLElement;
+
+  resultActiveLine: HTMLElement;
 
   constructor(params: ElementParametrs, appLoader: Loader) {
     super(params, appLoader);
-    this.panelViews = [null, null];
     this.resultPanelView = this.createResultPanel();
     this.wordsPanelView = this.createWordsPanel();
-    this.itemsOnClick();
+    this.wordsLines = [];
+    this.resultLines = [];
+    this.fillLinesProperties();
+    [this.wordsActiveLine] = this.wordsLines;
+    [this.resultActiveLine] = this.resultLines;
+    this.itemsPointerEvents();
+    this.itemsDragAndDrop();
   }
 
-  createResultPanel(): ResultPanelView {
+  private createResultPanel(): ResultPanelView {
     const RESULT_PANEL_PARAMS: ElementParametrs = {
       tag: "div",
       cssClasses: ["game-field__result-panel", "result-panel"],
@@ -31,82 +42,235 @@ export class GameFieldView extends ViewLoadable {
       this.appLoader,
     );
     this.getHtmlElement().append(resultPanel.getHtmlElement());
-    this.panelViews[0] = resultPanel;
     return resultPanel;
   }
 
-  createWordsPanel(): WordsPanelView {
+  private createWordsPanel(): WordsPanelView {
     const WORDS_PANEL_PARAMS: ElementParametrs = {
       tag: "div",
       cssClasses: ["game-field__words-panel", "words-panel"],
     };
     const wordsPanel = new WordsPanelView(WORDS_PANEL_PARAMS, this.appLoader);
     this.getHtmlElement().append(wordsPanel.getHtmlElement());
-    this.panelViews[1] = wordsPanel;
     return wordsPanel;
   }
 
-  itemsOnClick(): void {
+  private fillLinesProperties(): void {
     const wordsLines = [
       ...this.wordsPanelView.getHtmlElement().querySelectorAll(".line"),
     ];
     const resultLines = [
       ...this.resultPanelView.getHtmlElement().querySelectorAll(".line"),
     ];
-    function eventHandler(event: Event) {
-      const { currentTarget: line, target } = event;
-
-      if (!(target instanceof HTMLElement) || !(line instanceof HTMLElement)) {
-        throw new Error("currenTtarget or target is not HTMLelement");
-      } else if (line === null || target === null) {
-        throw new Error("cannot find target or current target");
+    wordsLines.forEach((line) => {
+      if (line instanceof HTMLElement) {
+        this.wordsLines.push(line);
       }
-
-      if (target === line) {
-        return;
-      }
-
-      target.ondragstart = () => false;
-
-      let item: HTMLElement;
-      if (
-        (target.classList.contains("item__text") ||
-          target.classList.contains("item__image")) &&
-        target.parentElement !== null
-      ) {
-        item = target.parentElement;
-      } else {
-        item = target;
-      }
-      if (item.style.opacity === "0") {
-        return;
-      }
-      const itemAnimator = new AnimationMaker(item);
-      if (line.classList.contains("result-panel__line")) {
-        wordsLines.forEach((wordsLine) => {
-          if (
-            wordsLine.classList.contains("words-panel__line_active") &&
-            wordsLine instanceof HTMLElement
-          ) {
-            itemAnimator.insert(wordsLine, "append");
-          }
-        });
-      } else {
-        resultLines.forEach((resultLine) => {
-          if (
-            resultLine.classList.contains("result-panel__line_active") &&
-            resultLine instanceof HTMLElement
-          ) {
-            itemAnimator.insert(resultLine, "append");
-          }
-        });
-      }
-    }
-    wordsLines.forEach((wordsLine) => {
-      wordsLine.addEventListener("click", eventHandler);
     });
-    resultLines.forEach((resultLine) => {
-      resultLine.addEventListener("click", eventHandler);
+    resultLines.forEach((line) => {
+      if (line instanceof HTMLElement) {
+        this.resultLines.push(line);
+      }
+    });
+  }
+
+  private itemsEventsSetup(event: Event): HTMLElement[] | null {
+    const { currentTarget: line, target } = event;
+    if (!(target instanceof HTMLElement) || !(line instanceof HTMLElement)) {
+      throw new Error("currenTtarget or target is not HTMLelement");
+    } else if (line === null || target === null) {
+      throw new Error("cannot find target or current target");
+    }
+    // disable events on inactive lines
+    if (
+      target === line ||
+      (line !== this.resultActiveLine && line !== this.wordsActiveLine)
+    ) {
+      return null;
+    }
+    // finding item
+    let item: HTMLElement;
+    if (
+      (target.classList.contains("item__text") ||
+        target.classList.contains("item__image")) &&
+      target.parentElement !== null
+    ) {
+      item = target.parentElement;
+    } else {
+      item = target;
+    }
+    // disable events on bleached(hidden) elements
+    if (item.style.opacity === "0") {
+      return null;
+    }
+    return [line, item];
+  }
+
+  private itemsPointerEvents(): void {
+    [...this.wordsLines, ...this.resultLines].forEach((Line) => {
+      Line.addEventListener("click", (event: Event) => {
+        const arr = this.itemsEventsSetup(event);
+        if (arr === null) {
+          return;
+        }
+        const [line, item] = arr;
+        const itemAnimator = new AnimationMaker(item);
+        const options = {
+          smoothDisappearance: true,
+        };
+        if (line.classList.contains("result-panel__line")) {
+          itemAnimator.insert(this.wordsActiveLine, "append", options);
+        } else {
+          itemAnimator.insert(this.resultActiveLine, "append", options);
+        }
+      });
+    });
+  }
+
+  private itemsDragAndDrop(): void {
+    [...this.wordsLines, ...this.resultLines].forEach((Line) => {
+      Line.addEventListener("pointerdown", (downEvent: PointerEvent) => {
+        const { target: pointerDownTarget } = downEvent;
+        if (pointerDownTarget instanceof HTMLElement) {
+          pointerDownTarget.ondragstart = () => false;
+          pointerDownTarget.onmousedown = () => false;
+        }
+        const arr = this.itemsEventsSetup(downEvent);
+        if (arr === null) {
+          return;
+        }
+        const [line, item] = arr;
+        if (item.getAttribute("insertAnimation") === "true") {
+          return;
+        }
+        const initCoordinates = item.getBoundingClientRect();
+        const itemAnimator = new AnimationMaker(item);
+        let pointerMoveHandler: (moveEvent: PointerEvent) => void;
+        let followPointerHandler: (moveEvent: PointerEvent) => void;
+        let pointerUpIsNeeded = false;
+        window.addEventListener(
+          "pointermove",
+          (pointerMoveHandler = (moveEvent: PointerEvent) => {
+            if (
+              Math.abs(downEvent.clientX - moveEvent.clientX) > 15 ||
+              Math.abs(downEvent.clientY - moveEvent.clientY) > 15
+            ) {
+              followPointerHandler = itemAnimator.startfollowPointer(
+                initCoordinates,
+                downEvent,
+              );
+              pointerUpIsNeeded = true;
+              window.removeEventListener("pointermove", pointerMoveHandler);
+            }
+          }),
+        );
+        const pointerUpHandler = (upEvent: PointerEvent) => {
+          window.removeEventListener("pointermove", pointerMoveHandler);
+          if (!pointerUpIsNeeded) {
+            return;
+          }
+          item.style.display = "none";
+          const elUnderMouse = document.elementFromPoint(
+            upEvent.clientX,
+            upEvent.clientY,
+          );
+          if (!(elUnderMouse instanceof HTMLElement)) {
+            return;
+          }
+          item.style.display = "";
+          let insertItem: HTMLElement | null;
+          if (
+            (elUnderMouse?.classList.contains("item__text") ||
+              elUnderMouse?.classList.contains("item__image")) &&
+            elUnderMouse.parentElement !== null &&
+            elUnderMouse.parentElement.style.opacity !== "0"
+          ) {
+            insertItem = elUnderMouse.parentElement;
+          } else if (elUnderMouse?.classList.contains("item")) {
+            insertItem = elUnderMouse;
+          } else {
+            insertItem = null;
+          }
+          if (insertItem !== null) {
+            const insertItemCoordinates = insertItem?.getBoundingClientRect();
+            itemAnimator.stopFollowPointer(followPointerHandler, {
+              smoothDisappearance: true,
+            });
+            let insertOptions: InsertOptions;
+            if (
+              insertItem.parentElement === line &&
+              insertItem.getBoundingClientRect().x > initCoordinates.x
+            ) {
+              // apend in same line more to the right than item
+              insertOptions = {
+                smoothDisappearance: true,
+                smoothInsert: true,
+                sameLaneAfterItem: true,
+              };
+            } else {
+              insertOptions = {
+                smoothDisappearance: true,
+                smoothInsert: true,
+              };
+            }
+            if (
+              upEvent.clientX - insertItemCoordinates.x >=
+              insertItemCoordinates.width / 2
+            ) {
+              itemAnimator.insert(insertItem, "after", insertOptions);
+            } else {
+              itemAnimator.insert(insertItem, "before", insertOptions);
+            }
+          } else if (
+            elUnderMouse === this.resultActiveLine ||
+            elUnderMouse === this.wordsActiveLine
+          ) {
+            if (elUnderMouse === line) {
+              itemAnimator.insert(elUnderMouse, "append", {
+                smoothInsert: true,
+                sameLaneAfterItem: true,
+              });
+            } else {
+              itemAnimator.insert(elUnderMouse, "append", {
+                smoothInsert: true,
+              });
+            }
+            itemAnimator.stopFollowPointer(followPointerHandler, {
+              smoothDisappearance: true,
+            });
+          } else {
+            let oppositLine: HTMLElement;
+            if (line === this.wordsActiveLine) {
+              oppositLine = this.resultActiveLine;
+            } else {
+              oppositLine = this.wordsActiveLine;
+            }
+            if (
+              Math.abs(
+                oppositLine.getBoundingClientRect().y -
+                  item.getBoundingClientRect().y,
+              ) *
+                2 <
+              Math.abs(
+                line.getBoundingClientRect().y - item.getBoundingClientRect().y,
+              )
+            ) {
+              itemAnimator.insert(oppositLine, "append", {
+                smoothDisappearance: true,
+              });
+              itemAnimator.stopFollowPointer(followPointerHandler, {
+                smoothDisappearance: true,
+              });
+            } else {
+              itemAnimator.stopFollowPointer(followPointerHandler, {
+                returnIsNeeded: true,
+              });
+            }
+          }
+          window.removeEventListener("pointerup", pointerUpHandler);
+        };
+        window.addEventListener("pointerup", pointerUpHandler);
+      });
     });
   }
 }
